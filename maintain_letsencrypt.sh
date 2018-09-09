b9d10a8d3789d82dba2b1dd7ab1b1e984a3e013e
Phil Massyn <massyn@users.noreply.github.com>
2018-09-09 15:12:35 +0900

    Update maintain_letsencrypt.sh

--- maintain_letsencrypt.sh
+++ maintain_letsencrypt.sh
@@ -6,6 +6,8 @@ if [[ `whoami` != 'root' ]]; then
         exit 1
 fi
 
-/usr/sbin/service apache2 stop
-/usr/local/letsencrypt/letsencrypt-auto renew
-/usr/sbin/service apache2 start
+if [[ -f /usr/local/letsencrypt/letsencrypt-auto ]]; then
+        /usr/sbin/service apache2 stop
+        /usr/local/letsencrypt/letsencrypt-auto renew
+        /usr/sbin/service apache2 start
+fi
