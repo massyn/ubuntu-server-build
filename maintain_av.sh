24c073d39ce94ece92717caf3c6588cb70199e8f
Phil Massyn <massyn@users.noreply.github.com>
2018-09-09 15:03:23 +0900

    Create maintain_av.sh

---
+++ maintain_av.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# == we must run as root
+if [[ `whoami` != 'root' ]]; then
+        echo "You need to run this as root";
+        exit 1
+fi
+
+# == check if clamav is installed
+dpkg -l clamav > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+        echo "Installing clamav..."
+        apt-get install clamav -y
+fi
+
+# == update the AV database
+/usr/bin/freshclam
+
+# == run the AV scan
+/usr/bin/clamscan -o -r -i /
